7b30466b94a2fa77bf1396b4351c756e7928e242
felipe <fe.moragal@duocuc.cl>
2024-11-28 20:05:28 -0300

    implementacion API nativa

--- src/app/foro/foro.page.ts
+++ src/app/foro/foro.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ForoService } from './foro.service';
 import { Topic } from './foro.model'; // Importa la interfaz
+import { Camera, CameraResultType, CameraSource, CameraPermissionType } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
+import { ActionSheetController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-foro',
@@ -17,8 +21,9 @@ export class ForoPage implements OnInit {
     description: ''
   };
   topics: Topic[] = [];
+  profileImage: string | null = null;
 
-  constructor(private foroService: ForoService) {}
+  constructor(private foroService: ForoService, private actionSheetController: ActionSheetController) {}
 
   ngOnInit() {
     this.loadTopics();
@@ -66,4 +71,60 @@ export class ForoPage implements OnInit {
       this.loadTopics();
     });
   }
+
+  async requestCameraPermissions() {
+    if (Capacitor.getPlatform() !== 'web') {
+      const permissions = await Camera.requestPermissions({
+        permissions: ['camera', 'photos']
+      });
+
+      if (permissions.camera === 'granted' && permissions.photos === 'granted') {
+        console.log('Permisos concedidos');
+      } else {
+        console.log('Permisos denegados');
+      }
+    } else {
+      console.log('La cámara no está disponible en la web.');
+    }
+  }
+
+  async selectImage() {
+    await this.requestCameraPermissions();
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Seleccionar Acción:',
+      buttons: [
+        {
+          text: 'Tomar Foto',
+          handler: () => {
+            this.getImage(CameraSource.Camera);
+          }
+        },
+        {
+          text: 'Seleccionar de la Galería',
+          handler: () => {
+            this.getImage(CameraSource.Photos);
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
+
+  async getImage(source: CameraSource) {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: source // Aquí se especifica la fuente de la imagen
+    });
+
+    if (image.dataUrl) {
+      this.profileImage = image.dataUrl;
+    }
+  }
+  
 }
